3b48e765c1d0c90d7adc65a063a20222250f3657
alberto-balsalm <wojciechpolitowski@protonmail.ch>
2020-04-20 14:24:26 +0200

    Added changing colours and drawing modes buttons

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ const newline = document.createElement('br')
 let colourMode = "grayscale"
 
 generateGrid()
-hoverEffect()
-changeColourMode()
+newGrid() // enables generating new grids
+hovering() // turns on hovering on default
+changeColourMode() //enables changing color modes
+changeDrawingMode() // enables changing drawing mode
+
 
 function generateGrid() {
     for(let i = 0; i < numSquares; i++) {
@@ -22,9 +25,60 @@ function generateGrid() {
     }
 }
 
-function hoverEffect() {
+function clearGrid() {
+    const squares = document.querySelectorAll(".square")
+    squares.forEach(square => grid.removeChild(square))
+}
+
+function newGrid() {    
+    const newGridButton = document.querySelector(".newGridButton")
+    newGridButton.addEventListener('click', () => {
+        numSquares = prompt("How many squares per side would you like (from 2 to 64)?")
+        console.log(numSquares)
+        if(numSquares === null || numSquares === "" || isNaN(numSquares)) {
+            alert("You should enter a number from 2 to 64")
+            return
+        }
+        else if(numSquares < 2 || numSquares > 64) {
+            alert("You entered a wrong number")
+            return 
+   
+        }
+        squareWidth = gridSize / numSquares
+        clearGrid()
+        generateGrid()
+        hovering()
+    })
+}
+
+function changeDrawingMode() {
+    const buttons = document.querySelectorAll('button')
+    buttons[2].onclick = () => {
+        hovering()
+        buttons[2].classList.add("buttonSelected")
+        buttons[3].classList.remove("buttonSelected")
+    }
+    buttons[3].onclick = () => {
+        clicking()
+        buttons[2].classList.remove("buttonSelected")
+        buttons[3].classList.add("buttonSelected")
+    }
+}
+
+function hovering() {
     const squares = document.querySelectorAll('.square')
-    squares.forEach(div => div.addEventListener('mouseover', changeColour))
+    squares.forEach(div => {
+        div.removeEventListener('mousedown', draw)
+        div.addEventListener('mouseover', draw)
+    })
+}
+
+function clicking() {
+    const squares = document.querySelectorAll('.square')
+    squares.forEach(div => {
+        div.removeEventListener('mouseover', draw)
+        div.addEventListener('mousedown', draw)
+    })
 }
 
 function changeColourMode() {
@@ -32,14 +86,18 @@ function changeColourMode() {
     //enable choosing grayscale mode
     buttons[0].addEventListener('click', () => {
         colourMode = "grayscale"
+        buttons[0].classList.add("buttonSelected")
+        buttons[1].classList.remove("buttonSelected")
     })
     //enable choosing random colour mode
     buttons[1].addEventListener('click', () => {
         colourMode = "random"
+        buttons[1].classList.add("buttonSelected")
+        buttons[0].classList.remove("buttonSelected")
     })
 }
 
-function changeColour(div) {
+function draw(div) {
     if(colourMode === "random") {
         div.target.style.opacity = 1
         div.target.style.backgroundColor = randomColour()
