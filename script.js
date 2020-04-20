89cc27e61f83f68d74b2001d3b9b50ad204799bb
alberto-balsalm <wojciechpolitowski@protonmail.ch>
2020-04-20 12:12:00 +0200

    Added buttons for color modes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const grid = document.querySelector(".grid")
-const gridSize = 800 
+const gridSize = 640 
 let numSquares = 16
 let squareWidth = gridSize / numSquares
 const newline = document.createElement('br')
-let colourMode = "black"
+let colourMode = "grayscale"
 
 generateGrid()
 hoverEffect()
+changeColourMode()
 
 function generateGrid() {
     for(let i = 0; i < numSquares; i++) {
@@ -26,21 +27,30 @@ function hoverEffect() {
     squares.forEach(div => div.addEventListener('mouseover', changeColour))
 }
 
+function changeColourMode() {
+    const buttons = document.querySelectorAll('button')
+    //enable choosing grayscale mode
+    buttons[0].addEventListener('click', () => {
+        colourMode = "grayscale"
+    })
+    //enable choosing random colour mode
+    buttons[1].addEventListener('click', () => {
+        colourMode = "random"
+    })
+}
+
 function changeColour(div) {
-    if(colourMode === "random") div.target.style.backgroundColor = randomColour()
-    else if(colourMode === "black") {
-        console.log(div.target.style.backgroundColor)
+    if(colourMode === "random") {
+        div.target.style.opacity = 1
+        div.target.style.backgroundColor = randomColour()
+    } else if(colourMode === "grayscale") {
         if(div.target.style.backgroundColor === "rgb(0, 0, 0)") {
-            console.log(div.target.style.opacity)
             div.target.style.opacity = parseFloat(div.target.style.opacity) + 0.1
         }
         else {
             div.target.style.backgroundColor = "#000000"
             div.target.style.opacity = 0.1
-        }
-        
-
-        
+        }          
     }
 }
 
